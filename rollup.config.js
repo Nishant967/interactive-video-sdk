28bf2d58f413005654746b2009d91e07d1e85721
Nishant Rawat <rawat.nishant967@gmail.com>
2024-09-25 11:12:02 +0530

    update gitignore and config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,44 @@
-import typescript from 'rollup-plugin-typescript2';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+// rollup.config.js
 
+// Import necessary Rollup plugins
+import typescript from 'rollup-plugin-typescript2'; // Plugin for TypeScript support
+import resolve from '@rollup/plugin-node-resolve'; // Plugin to resolve node modules
+import commonjs from '@rollup/plugin-commonjs'; // Plugin to convert CommonJS modules to ES6
+import terser from '@rollup/plugin-terser'; // Plugin for minifying the output
+
+// Export Rollup configuration
 export default {
   // Entry point for the bundle
   input: 'src/InteractiveVideoSDK.ts',
-  output: {
-    // Output file configuration
-    file: 'dist/interactive-video-sdk.js',
-    format: 'umd', // Universal Module Definition format
-    name: 'InteractiveVideoSDK' // Global variable name for UMD build
-  },
+
+  // Output configuration for the bundle
+  output: [
+    {
+      file: 'dist/interactive-video-sdk.js', // Output file path for UMD build
+      format: 'umd', // Universal Module Definition format
+      name: 'InteractiveVideoSDK', // Global variable name for UMD build
+      sourcemap: true, // Generate source maps for debugging
+    },
+    {
+      file: 'dist/interactive-video-sdk.min.js', // Output file path for minified UMD build
+      format: 'umd', // Universal Module Definition format
+      name: 'InteractiveVideoSDK', // Global variable name for UMD build
+      plugins: [terser()], // Minify the output using terser
+      sourcemap: true, // Generate source maps for debugging
+    },
+    {
+      file: 'dist/interactive-video-sdk.esm.js', // Output file path for ES module build
+      format: 'es', // ES module format
+      sourcemap: true, // Generate source maps for debugging
+    },
+  ],
+
+  // Plugins to be used in the build process
   plugins: [
     resolve(), // Allows Rollup to resolve modules from node_modules
     commonjs(), // Converts CommonJS modules to ES6
-    typescript() // Compiles TypeScript files
-  ]
+    typescript({ // Compiles TypeScript files
+      useTsconfigDeclarationDir: true, // Use the declaration directory specified in tsconfig
+    }),
+  ],
 };
